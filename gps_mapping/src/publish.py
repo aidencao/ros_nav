bac19393f41aefb695e09a033dd40ac5c70048f9
Aiden <aiden_cao@outlook.com>
2020-05-21 09:59:54 +0800

    修改格式

--- gps_mapping/src/publish.py
+++ gps_mapping/src/publish.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         rospy.loginfo("Starting publish_node")
 
         gps_pub = rospy.Publisher("odom", Odometry,
-                                      queue_size=1)  # 用于向串口发送GPS路径
+                                  queue_size=1)  # 用于向串口发送GPS路径
 
         odom = Odometry()
         odom.header.stamp = rospy.Time.now()
@@ -18,12 +18,12 @@ if __name__ == '__main__':
         odom.pose.pose.position.x = 11
         odom.pose.pose.position.y = 11
         odom.pose.pose.position.z = 10
-        
+
         odom.child_frame_id = "base_link"
         odom.twist.twist.linear.x = 0.0
         odom.twist.twist.linear.y = 0.0
         odom.twist.twist.angular.z = 0.0
-        
+
         gps_pub.publish(odom)
 
     except KeyboardInterrupt:
